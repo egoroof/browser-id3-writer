7f42861a60e0ada30a0ca6ac8acea66bb8bdd11b
Artyom Egorov <mail@egoroof.ru>
2016-05-16 12:42:35 +0800

    move babel configuration

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,10 @@ module.exports = (config) => {
                     {
                         test: /\.js$/,
                         exclude: /node_modules/,
-                        loader: 'babel-loader'
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ['es2015']
+                        }
                     }
                 ]
             }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,10 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                }
             }
         ]
     },
