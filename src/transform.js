ccb206e694f4ab0a5692e300a1933d7c04fa14d3
Artyom Egorov <mail@egoroof.ru>
2016-12-26 23:04:36 +0800

    make transform functions more understandable

--- src/transform.js
+++ src/transform.js
@@ -1,28 +1,32 @@
 export function uint32ToUint8Array(uint32) {
-    const uint8array = [];
     const eightBitMask = (1 << 8) - 1;
 
-    for (let i = 24; i >= 0; i -= 8) {
-        uint8array.push((uint32 >>> i) & eightBitMask);
-    }
-    return uint8array;
+    return [
+        (uint32 >>> 24) & eightBitMask,
+        (uint32 >>> 16) & eightBitMask,
+        (uint32 >>> 8) & eightBitMask,
+        uint32 & eightBitMask
+    ];
 }
 
 export function uint28ToUint7Array(uint28) {
-    const uint7array = [];
     const sevenBitMask = (1 << 7) - 1;
 
-    for (let i = 21; i >= 0; i -= 7) {
-        uint7array.push((uint28 >>> i) & sevenBitMask);
-    }
-    return uint7array;
+    return [
+        (uint28 >>> 21) & sevenBitMask,
+        (uint28 >>> 14) & sevenBitMask,
+        (uint28 >>> 7) & sevenBitMask,
+        uint28 & sevenBitMask
+    ];
 }
 
 export function uint7ArrayToUint28(uint7Array) {
     let uint28 = 0;
 
-    for (let i = 0, pow = 21; pow >= 0; pow -= 7, i++) {
-        uint28 += uint7Array[i] << pow;
-    }
+    uint28 += uint7Array[0] << 21;
+    uint28 += uint7Array[1] << 14;
+    uint28 += uint7Array[2] << 7;
+    uint28 += uint7Array[3];
+
     return uint28;
 }
