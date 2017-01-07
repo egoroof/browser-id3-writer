f953cdb3cf9333356512fafb4c75ce2b3746ad9e
Artyom Egorov <mail@egoroof.ru>
2017-01-07 12:59:03 +0800

    update prepare_readme.js

--- tools/prepare_readme.js
+++ tools/prepare_readme.js
@@ -2,17 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const crypto = require('crypto');
 const pack = require('../package.json');
+
 const rootDir = path.dirname(__dirname);
-const readmeDraft = fs.readFileSync(path.join(rootDir, 'tools', 'README.draft.md'), 'utf8');
+const readmePath = path.join(rootDir, 'README.md');
+const readmeDraftPath = path.join(rootDir, 'tools', 'README.draft.md');
+const distFilePath = path.join(rootDir, 'dist', 'browser-id3-writer.min.js');
+const algo = 'sha384';
 
-const hash = crypto.createHash('sha384');
-hash.update(fs.readFileSync(path.join(rootDir, 'dist', 'browser-id3-writer.min.js'), 'utf8'));
+const readmeDraft = fs.readFileSync(readmeDraftPath, 'utf8');
+const hash = crypto.createHash(algo);
+hash.update(fs.readFileSync(distFilePath, 'utf8'));
 const digest = hash.digest('base64');
-
 const readme = readmeDraft
     .replace('###version###', pack.version)
-    .replace('###hash###', `sha384-${digest}`);
+    .replace('###hash###', `${algo}-${digest}`);
+fs.writeFileSync(readmePath, readme);
 
-fs.writeFileSync(path.join(rootDir, 'README.md'), readme);
-
-console.log(`README.md is updated, version: ${pack.version}, hash: ${digest}`);
+console.log(`${readmePath} is updated, version: ${pack.version}, hash: ${algo}-${digest}`);
