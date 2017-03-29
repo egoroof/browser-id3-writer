b88db19ef9fe2b13103d77f5f64f6f8bdbd72f84
Artyom Egorov <mail@egoroof.ru>
2017-03-29 20:59:31 +0700

    remove duplication from encoder

--- src/encoder.js
+++ src/encoder.js
@@ -1,16 +1,16 @@
 // https://encoding.spec.whatwg.org/
 
-export function encodeWindows1252(str) {
-    const codePoints = String(str).split('').map((c) => c.charCodeAt(0));
+function strToCodePoints(str) {
+    return String(str).split('').map((c) => c.charCodeAt(0));
+}
 
-    return new Uint8Array(codePoints);
+export function encodeWindows1252(str) {
+    return new Uint8Array(strToCodePoints(str));
 }
 
 export function encodeUtf16le(str) {
-    const codePoints = String(str).split('').map((c) => c.charCodeAt(0));
     const output = new Uint8Array(str.length * 2);
-
-    new Uint16Array(output.buffer).set(codePoints);
+    new Uint16Array(output.buffer).set(strToCodePoints(str));
 
     return output;
 }
