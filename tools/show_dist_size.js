36706dabdc475825476f6f6468a1d1905e4f700e
Artyom Egorov <mail@egoroof.ru>
2018-01-08 15:43:05 +0700

    Some cleanup

--- tools/show_dist_size.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-const fs = require('fs');
-const path = 'dist/browser-id3-writer.js';
-
-const stat = fs.statSync(path);
-const size = (stat.size / 1024).toFixed(2);
-
-console.log(`Size of "${path}" is ${size} KiB`);
